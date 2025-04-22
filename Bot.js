0ee38083c3d8107d30e431464ca7c8c4cbce3b4e
KRISHDEVELOPER123 <daspiyush128@gmail.com>
2025-04-22 11:40:33 +0530

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,1 +1,31 @@
+const mineflayer = require('mineflayer');
 
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'yourserver.aternos.me',
+    port: 25565,
+    username: 'AFK_Bot'
+  });
+
+  bot.on('spawn', () => {
+    console.log('Bot joined the server and is AFK + Moving!');
+    bot.setControlState('jump', true);
+    bot.setControlState('forward', true);
+    setInterval(() => {
+      const direction = Math.random() > 0.5 ? 'left' : 'right';
+      bot.setControlState('left', direction === 'left');
+      bot.setControlState('right', direction === 'right');
+    }, 10000);
+  });
+
+  bot.on('end', () => {
+    console.log('Bot disconnected. Reconnecting...');
+    setTimeout(createBot, 5000);
+  });
+
+  bot.on('error', (err) => {
+    console.log('Error:', err);
+  });
+}
+
+createBot();
